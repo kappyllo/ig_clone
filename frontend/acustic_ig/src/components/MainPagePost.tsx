968f725d18ce75ab6cf820ecd12caaadcd605237
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-01-03 10:01:18 +0100

    working on posts

---
+++ frontend/acustic_ig/src/components/MainPagePost.tsx
@@ -0,0 +1,36 @@
+interface Props {
+  op: { img: string; nickName: string };
+  postTime: string;
+  description: string;
+  photo: string;
+  likes: number;
+  comments: { count: number; comms: string[] };
+}
+
+export default function MainPagePost({
+  op,
+  postTime,
+  description,
+  photo,
+  likes,
+  comments,
+}: Props) {
+  return (
+    <div>
+      <div className="flex mb-2">
+        <img className="rounded-full w-9    " src={op.img} alt="" />
+        <p>{op.nickName}</p>
+        <p>{postTime}</p>
+        <button>...</button>
+      </div>
+      <img src={photo} alt="" />
+      <div>
+        <p>Liczba polubień: {likes}</p>
+        <p>
+          {op.nickName} {description}
+        </p>
+        <p>Zobacz wszystkie komentarze: {comments.count}</p>
+      </div>
+    </div>
+  );
+}
--- frontend/acustic_ig/src/components/Posts.tsx
+++ frontend/acustic_ig/src/components/Posts.tsx
@@ -1,3 +1,31 @@
+import MainPagePost from "./MainPagePost";
+
+const EXAMPLE_POSTS = [
+  {
+    op: { nickName: "Kacper", img: "cat-story.jpeg" },
+    postTime: "12 godz.",
+    description: "ciekawy opis",
+    likes: 2,
+    comments: { count: 1, comms: ["test"] },
+    photo: "src/imgs/post_img.jpg",
+  },
+];
+
 export default function Posts() {
-  return null;
+  return (
+    <>
+      {EXAMPLE_POSTS.map((post) => {
+        return (
+          <MainPagePost
+            op={post.op}
+            postTime={post.postTime}
+            description={post.description}
+            likes={post.likes}
+            comments={post.comments}
+            photo={post.photo}
+          />
+        );
+      })}
+    </>
+  );
 }
