312881dc7d0da05c0bcd3eb7da67b7d435e8ac61
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-01-12 20:38:15 +0100

    Finnaly fixed storys on mobile.

--- frontend/acustic_ig/src/UI/StoryBtn.tsx
+++ frontend/acustic_ig/src/UI/StoryBtn.tsx
@@ -6,7 +6,11 @@ interface Props {
 export default function StoryBtn({ nickName, img }: Props) {
   return (
     <button className="flex flex-col items-center justify-center">
-      <img className="w-12 rounded-full" src={img} alt="profile picture" />
+      <img
+        className="w-12 rounded-full sm:max-w-none"
+        src={img}
+        alt="profile picture"
+      />
       <p>{nickName}</p>
     </button>
   );
