bbe6f3db5f656a502dadd5aa2537ae26a6b84738
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-01-13 09:49:26 +0100

    Adding hover style to left nav bar.

--- frontend/acustic_ig/src/UI/LeftSideBarBtn.tsx
+++ frontend/acustic_ig/src/UI/LeftSideBarBtn.tsx
@@ -5,8 +5,8 @@ interface Props {
 
 export default function LeftSideBarBtn({ icon, text }: Props) {
   return (
-    <button className="flex items-center mb-5">
-      <img className="mr-3" src={icon} alt="" />
+    <button className="flex items-center mb-5 justify-center text-center">
+      <img className="mr-3 pl-2" src={icon} alt="" />
       <p>{text}</p>
     </button>
   );
