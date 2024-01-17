e4bf263307ee7eafab43b1a43b5062a0cc64b5a7
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-01-17 10:09:55 +0100

    Working on log-in page.

--- frontend/acustic_ig/src/pages/LoginPage.tsx
+++ frontend/acustic_ig/src/pages/LoginPage.tsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex flex-col justify-center h-screen items-center">
+      <h1 className="mb-5 text-3xl">𝒜𝒸𝓊𝓈𝓉𝒾𝒸𝑔𝓇𝒶𝓂</h1>
+      <div className="flex flex-col">
+        <input
+          className="mb-2 w-64 border p-2"
+          type="text"
+          placeholder="Nickname"
+          name=""
+          id=""
+        />
+        <input
+          className="mb-3 border p-2"
+          type="password"
+          placeholder="Password"
+          name=""
+          id=""
+        />
+        <button className="bg-blue-400 text-white rounded font-semibold p-1">
+          <Link to={"/"}>Log in</Link> {/* // to change later */}
+        </button>
+        <p className="text-center">------ OR -------</p>
+        <button className="text-blue-400 font-semibold">Register</button>
+      </div>
+    </div>
+  );
 }
