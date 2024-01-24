056cc663db072e5c105eecb9615405d764cf3536
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-01-24 18:01:27 +0100

    Adding photos grid-component in profile.

--- frontend/acustic_ig/src/components/ProfilePhotos.tsx
+++ frontend/acustic_ig/src/components/ProfilePhotos.tsx
@@ -1,3 +1,24 @@
+const EXAMPLE_POSTS = [
+  { photo: "cat-story.jpeg" },
+  { photo: "cat-story.jpeg" },
+  { photo: "cat-story.jpeg" },
+  { photo: "cat-story.jpeg" },
+];
+
 export default function ProfilePhotos() {
-  return null;
+  return (
+    <div className="flex justify-center mt-12">
+      <ul className="grid gap-1 grid-cols-3">
+        {EXAMPLE_POSTS.map((post) => {
+          return (
+            <li>
+              <button>
+                <img className="w-56" src={post.photo} alt="" />
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
